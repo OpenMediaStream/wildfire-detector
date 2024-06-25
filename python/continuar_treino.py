03ac1cd7c9a5482cea895dc867203a67c1e8a7b3
Vinicius Parelho <vinicius.oliveira@maua.br>
2024-06-25 15:24:05 -0300

    caixas adicionadas na imagem do relatório

--- python/continuar_treino.py
+++ python/continuar_treino.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 model = YOLO('./runs/detect/train/weights/last.pt')
 
-results = model.train(data='data_custom.yaml', epochs=50, batch=0.90, resume=True, amp=True)
+results = model.train(data='data_custom.yaml', epochs=50, batch=0.90, resume=True)
--- python/relatorio.py
+++ python/relatorio.py
@@ -5,6 +5,7 @@ from io import BytesIO
 import numpy as np
 import cv2
 
+# Connect to DB
 load_dotenv()
 host = os.getenv('DB_HOST')
 user = os.getenv('DB_USER')
@@ -20,14 +21,34 @@ connection = cnn.connect(
 
 cursor = connection.cursor()
 
+# Obtain image binary data from longblob and coords string
 cursor.execute("SELECT image FROM wildfire WHERE id = 1")
 image_data = cursor.fetchone()[0]
+
+cursor.execute("SELECT coords from wildfire WHERE id = 1")
+coords_string = cursor.fetchone()[0]
 
 cursor.close()
 connection.close()
 
+# Turns coords into a list and decodes the binary image data
 image_array = np.frombuffer(image_data, dtype=np.uint8)
+image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
 
-image_cv2 = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+coords = coords_string.split('|')
+while '' in coords:
+    coords.remove('')
 
-cv2.imwrite('imagem_db.jpg', image_cv2)
+
+# Draws the boxes on the image to save the final result
+parsed_coordinates = []
+for coord in coords:
+    x1, y1, x2, y2 = map(float, coord.split(','))
+    parsed_coordinates.append((int(x1), int(y1), int(x2), int(y2)))
+
+for (x1, y1, x2, y2) in parsed_coordinates:
+    cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 255), 4)
+
+cv2.imwrite('result.jpg', image)
+
+# Adicionar tags nas caixas para ficar igual ao plot do YOLO
